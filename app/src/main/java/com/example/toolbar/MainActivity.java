336a32ff45de2e2ffd24f468e1271d1ac3207437
khalid063 <khalid.ahmad063@gmail.com>
2021-03-08 16:16:07 +0600

    second commit

--- app/src/main/java/com/example/toolbar/MainActivity.java
+++ app/src/main/java/com/example/toolbar/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_id);
+        setSupportActionBar(toolbar);
+
+        TextView textView = findViewById(R.id.tv_toolbar_headline);
+        textView.setText("Home");
+
+        Button button = findViewById(R.id.but_activity_change);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Toast.makeText(MainActivity.this, "Tost", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/toolbar/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+//        Toolbar toolbar = findViewById(R.id.toolbar_id);
+//        setSupportActionBar(toolbar);
+        TextView textView = findViewById(R.id.tv_toolbar_headline);
+        textView.setText("Page Two");
+        //getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+}
